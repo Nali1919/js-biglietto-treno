ec9a013ad26f1659dea71f46962f768967bd5941
Nali1919 <my@email.com>
2022-08-23 16:35:55 +0200

    Creo struttura e prime const

---
+++ js/script.js
@@ -0,0 +1,12 @@
+console.log('JS OK!');
+
+const km = prompt('Quanti km vuoi percorrere?');
+const kmAsNumber = parseInt(km);
+console.log('km percorsi', kmAsNumber);
+
+const età = prompt('Quanti anni hai');
+const etàAsNumber = parseInt(età);
+console.log('età', etàAsNumber);
+
+
+
