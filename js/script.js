5c52ecb1560208c687a0ad5c9dbb3833e59eacff
Nali1919 <my@email.com>
2022-08-23 16:59:14 +0200

    Aggiungo prezzo

--- js/script.js
+++ js/script.js
@@ -4,9 +4,18 @@ const km = prompt('Quanti km vuoi percorrere?');
 const kmAsNumber = parseInt(km);
 console.log('km percorsi', kmAsNumber);
 
+
 const età = prompt('Quanti anni hai');
 const etàAsNumber = parseInt(età);
 console.log('età', etàAsNumber);
+
+const prezzo = 0.21 * kmAsNumber
+const risposta = `Il prezzo è ${prezzo}`;
+
+document.getElementById('mio_id').innerHTML= risposta;
+
+
+
 
 
 
